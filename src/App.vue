a4e8781a7754f78b0de9fa5f81d8ffd60e903ded
nan[ф] <56279981+nonobstant@users.noreply.github.com>
2024-01-29 00:31:03 +0100

    elvizo website

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="bg-gray-200 dark:bg-gray-900 dark:text-white min-h-screen text-center pt-5 transition duration-700 ease-in-out">
-    <svg v-if='theme ==="light"'  @click="themeDark" class="w-10 h-10 inline cursor-crosshair p-2 hover:text-white hover:bg-black rounded-lg" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-     <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-    </svg>
-    <svg v-else  @click="themeLight" class="w-10 h-10 inline cursor-crosshair p-2 hover:text-black hover:bg-white rounded-lg" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-     <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-    </svg>
-    <img class="mx-auto h-56" alt="Vue logo" src="./assets/nox.png" />
-    <h1 class="text-xl font-bold">[ nox ] <span class="text-teal-700">nonobstant</span></h1>
-    <svg class="w-6 h-6 inline mt-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd"></path>
-    </svg>
+  <div class="bg-gray-200 dark:bg-gray-900 dark:text-white min-h-screen text-center pt-5 transition duration-700 ease-in-out ">
+    <div class="grid grid-cols-2 mx-auto max-w-xl my-5">
+      <div>
+        <h1 class="text-base text-slate-800 font-semibold text-left"><span class="text-black font-bold text-xl dark:text-white mt-2 mx-2">Elvizo</span> Football betting tips</h1>
+      </div>
+      <div class="text-right">
+          <svg v-if='theme ==="light"'  @click="themeDark" class="w-10 h-10 inline cursor-crosshair p-2 hover:text-white hover:bg-black rounded-lg" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        </svg>
+        <svg v-else  @click="themeLight" class="w-10 h-10 inline cursor-crosshair p-2 hover:text-black hover:bg-white rounded-lg" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        </svg>
+      </div>
+    </div>
     <router-view></router-view>
-    <h1 class="mt-16">Made with ❤ by nonobstant</h1>
+    <h1 class="py-3">Made by <span class="cursor-pointer hover:underline font-semibold">MatchCorner</span></h1>
   </div>
   
 </template>
