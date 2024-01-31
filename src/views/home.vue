3e08ed48db2c1de8d64711d6fa63b1b3c81e7158
nan[ф] <56279981+nonobstant@users.noreply.github.com>
2024-01-31 20:55:21 +0100

    button

--- src/views/home.vue
+++ src/views/home.vue
@@ -13,8 +13,8 @@
         </div>
       </h1>
       <div v-if="index < 1" class="flex justify-between my-6">
-        <p class="px-2 py-1 border-2 border-slate-800 dark:border-slate-200">BONUS: <span class="font-mono font-semibold">ELVIZO</span></p>
-        <p class="px-2 font-semibold py-1 border-2 border-slate-800 dark:border-slate-200">OVER ONE GOAL BET</p>
+        <p class="px-2 rounded-lg py-1 border-4 border-slate-800 dark:border-slate-200 hover:bg-slate-800 cursor-pointer hover:text-slate-200 dark:hover:bg-slate-200 dark:hover:text-slate-800">BONUS: <span class="font-mono font-semibold">ELVIZO</span></p>
+        <p class="px-2 rounded-lg font-semibold py-1 border-4 border-slate-800 dark:border-slate-200 hover:bg-slate-800 cursor-pointer hover:text-slate-200 dark:hover:bg-slate-200 dark:hover:text-slate-800">OVER ONE GOAL BET</p>
       </div>
     </div>
   </div>
