9622d33788da521ed2d8fe86bc16ecde009fb393
nan[ф] <56279981+nonobstant@users.noreply.github.com>
2024-02-06 21:28:07 +0100

    day

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,8 @@
       <svg v-else  @click="themeLight" class="w-6 h-6 inline cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path fill="currentColor" d="M12 6a6 6 0 0 1 6 6c0 2.22-1.21 4.16-3 5.2V19a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-1.8c-1.79-1.04-3-2.98-3-5.2a6 6 0 0 1 6-6m2 15v1a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-1zm6-10h3v2h-3zM1 11h3v2H1zM13 1v3h-2V1zM4.92 3.5l2.13 2.14l-1.42 1.41L3.5 4.93zm12.03 2.13l2.12-2.13l1.43 1.43l-2.13 2.12z"/>
       </svg>
-      <h1 class="py-3 mt-2">Copyright <span class="font-mono">2024</span> @<a href="https://www.youtube.com/@ELVIZO" class="cursor-pointer inline hover:underline underline-offset-4 font-semibold">ELVIZO</a></h1>
+      <h1 class=" mt-5">www.elvizo.net <a href="/privacy-policy" class="cursor-pointer inline hover:underline underline-offset-4 font-semibold">Privacy Policy</a> page</h1>
+      <h1 class="py-2">Copyright <span class="font-mono">2024</span> @<a href="https://www.youtube.com/@ELVIZO" class="cursor-pointer inline hover:underline underline-offset-4 font-semibold">ELVIZO</a></h1>
     </footer>
   </div>
   
@@ -53,8 +54,7 @@ export default {
     //theme setup
     if (
       localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
+      (!("theme" in localStorage))
     ) {
       document.documentElement.classList.add("dark");
       this.theme = "dark";
